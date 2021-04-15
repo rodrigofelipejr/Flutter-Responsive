f010bb698a44556cf41aef547af76e43f3b3c4d3
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-04-14 22:18:09 -0300

    Utilizando o AutoSizeText

--- flutter_app/lib/pages/home/widgets/course_item.dart
+++ flutter_app/lib/pages/home/widgets/course_item.dart
@@ -1,39 +1,59 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class CourseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Padding(
-          padding: const EdgeInsets.only(bottom: 4.0),
+          padding: const EdgeInsets.only(bottom: 8.0),
           child: Image.asset(
             "assets/images/image-2.jpg",
             fit: BoxFit.fitWidth,
           ),
         ),
-        Text(
-          "Criação de Apps Android e iOS com Flutter - Crie 16 Apps",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16.0,
-            color: Colors.white,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 6.0),
-          child: Text(
-            "Daniel Ciolfi",
-            style: TextStyle(color: Colors.grey),
-          ),
-        ),
-        Text(
-          "R\$ 22,90",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 14.0,
-            color: Colors.white,
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      print(constraints.biggest);
+
+                      return AutoSizeText(
+                        "Criação de Apps Android e iOS com Flutter - Crie 16 Apps",
+                        minFontSize: 1,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                          color: Colors.white,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6.0),
+                  child: Text(
+                    "Daniel Ciolfi",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                Text(
+                  "R\$ 22,90",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
